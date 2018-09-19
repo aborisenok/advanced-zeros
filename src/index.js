f4ed483e5e6b581968da736cc15a6c93920ea0cf
aborisenok <lesha_borisenok@mail.ru>
2018-09-19 11:26:03 +0300

    well done this time

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
 module.exports = function getZerosCount(number, base) {
   var result = 0;
-  var degree;
-  var val;
-  if(isPrime(base)){
-    val = base;
-    degree = 1;
-  } else {
-    var primeFactors = findPrimeFactors(base);
-    var numberOfMaxPrime = findNumberOfMax(primeFactors);
-    val = numberOfMaxPrime[0];
-    degree = numberOfMaxPrime[1];
-  } 
-  while(number > 1){
-    number = number/val;
-    result = result + Math.floor(number);
+  var primes = findPrimeFactors(base);
+  var primeKeys = Object.keys(primes);
+
+  for(let i = 0; i < primeKeys.length; i++){
+    var diver = primeKeys[i];
+    var degree =  primes[primeKeys[i]];
+    var tempRes = 0;
+
+    while(diver < number){
+      tempRes += Math.floor(number/diver);
+      diver *= primeKeys[i];
+    }
+    
+    tempRes = Math.floor(tempRes/degree);
+    
+    if(result){
+      if(tempRes <= result){
+        result = tempRes; 
+      } 
+    }else{
+      result = tempRes;
+    }
   }
-  return Math.floor(result/degree);
+
+  return result;
 }
 
-function isPrime(num) {
-  if(num <= 1) return false;
-  if(num === 2) return true;
-  if(num % 2 === 0 && num !== 2) return false;
-  var sqrt = Math.ceil(Math.sqrt(num));
-  for(let i = 2; i <= sqrt; i++){
-    if(num % i === 0){
-      return false;
-    } 
-  }
-  return true;
-}
 
 function findPrimeFactors(number){
   var arrOfPrime = [];
@@ -39,14 +36,15 @@ function findPrimeFactors(number){
       number = number/i;
     }
   }
+
+  var res = findNumberOfMax(arrOfPrime);
   
-  return arrOfPrime;
+  return res;
 }
 
 function findNumberOfMax(array){
   var map = {};
-  var result = [];
-  
+
   for(let i = 0; i < array.length; i++){
     if(map[array[i]]){
       map[array[i]]++;
@@ -54,20 +52,5 @@ function findNumberOfMax(array){
       map[array[i]] = 1;
     }
   }
-  
-  for(key in map){
-    if(result.length){
-      
-      if(key > result[1] ){
-        result[0] = +key;
-        result[1] = map[key];
-      }
-      
-    }else{
-      result[0] = +key;
-      result[1] = map[key];
-    }
-  }
-  
-  return result;
+  return map;
 }
